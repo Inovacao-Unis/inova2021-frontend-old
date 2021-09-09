a1b6a1f4c6f7a459b3f6ef524ce1b95d7438715c
patrickpereirak <patrickpereirak@gmail.com>
2021-09-09 14:59:12 -0300

    get journey from backend

--- pages/minha-conta.js
+++ pages/minha-conta.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+import api from '@services/api';
 import Layout from '@components/Layout';
 import withAuth from '@components/withAuth';
 import Link from 'next/link';
@@ -11,66 +13,89 @@ import {
   Button,
 } from '@chakra-ui/react';
 
-const minhaConta = () => (
-  <Layout profile>
-    <Container maxW="container.xl">
-      <Heading
-        fontSize="2.5rem"
-        fontWeight="700"
-        textAlign="center"
-        m="100px auto"
-      >
-        minhas jornadas
-      </Heading>
-      <Grid
-        templateColumns="repeat(auto-fit, minmax(275px, 300px))"
-        gap="100px"
-      >
-        <Link href="/jornada/design-thinking">
-          <Flex
-            direction="column"
-            justify="center"
-            align="center"
-            textAlign="center"
-            borderRadius="10px"
-            mt="10px"
-            mb="80px"
-            mx="auto"
-            bgColor="white"
-            color="black"
-            cursor="pointer"
-          >
-            <Image
-              src="https://loja.belasartes.br/portal-ecom/wp-content/uploads/2020/03/Startup-Thinking-scaled.jpg"
-              alt="Imagem da jornada"
-              borderRadius="10px 10px 0 0"
-              minH="196px"
-            />
-            <Flex direction="column" p="40px">
-              <Text
-                fontSize="1.4rem"
-                lineHeight="20px"
-                fontWeight="700"
-                mb="16px"
-              >
-                Design Thinking
-              </Text>
-              <Text lineHeight="130%" mb="16px" minH="64px" maxH="8rem">
-                Uma jornada incrível para desenvolver sua Startup
-              </Text>
-              <Button
-                bgColor="highlight"
-                color="white"
-                _hover={{ bg: 'highlight' }}
-              >
-                Acessar
-              </Button>
-            </Flex>
-          </Flex>
-        </Link>
-      </Grid>
-    </Container>
-  </Layout>
-);
+const minhaConta = () => {
+  const [journeys, setJourneys] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      await api.get('journeys').then((res) => setJourneys(res.data));
+    };
+
+    getData();
+  }, []);
+
+  return (
+    <Layout profile>
+      <Container maxW="container.xl" zIndex="800">
+        <Heading
+          fontSize="2.5rem"
+          fontWeight="700"
+          textAlign="center"
+          m="100px auto"
+        >
+          minhas jornadas
+        </Heading>
+        <Grid
+          templateColumns="repeat(auto-fit, minmax(275px, 300px))"
+          gap="100px"
+        >
+          {journeys && journeys.length > 0 ? (
+            journeys.map((journey) => (
+              // eslint-disable-next-line no-underscore-dangle
+              <Link key={journey._id} href={`/jornada/${journey.slug}`}>
+                <Flex
+                  direction="column"
+                  justify="center"
+                  align="center"
+                  textAlign="center"
+                  borderRadius="10px"
+                  mt="10px"
+                  mb="80px"
+                  mx="auto"
+                  bgColor="white"
+                  color="black"
+                  cursor="pointer"
+                >
+                  <Image
+                    src={
+                      journey?.image
+                        ? journey.image
+                        : '/images/journey-placeholder.jpg'
+                    }
+                    alt="Imagem da jornada"
+                    borderRadius="10px 10px 0 0"
+                    minH="196px"
+                  />
+                  <Flex direction="column" p="40px">
+                    <Text
+                      fontSize="1.4rem"
+                      lineHeight="20px"
+                      fontWeight="700"
+                      mb="16px"
+                    >
+                      {journey.title}
+                    </Text>
+                    <Text lineHeight="130%" mb="16px" minH="64px" maxH="8rem">
+                      {journey.description}
+                    </Text>
+                    <Button
+                      bgColor="highlight"
+                      color="white"
+                      _hover={{ bg: 'highlight' }}
+                    >
+                      Acessar
+                    </Button>
+                  </Flex>
+                </Flex>
+              </Link>
+            ))
+          ) : (
+            <Text>Nenhuma jornada</Text>
+          )}
+        </Grid>
+      </Container>
+    </Layout>
+  );
+};
 
 export default withAuth(minhaConta);
